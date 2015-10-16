1d120b22803406bf1926f0666c9289589333c924
David Gourde <gourde.david@gmail.com>
2015-10-16 12:38:37 -0400

    Ajout de la fonction déplacement et correction de nombreux problèmes. TODO: 1. Changer le mur de SKShapeNode à son sprite de mur et seulement appliquer une rotation/stretch sur le node. 2. Trouver une manière de tester si je clique dans la boite englobante ET NON dans le sprite pour la sélection des objets. 3. Empêcher les déplacements hors de la table. Pareil pour le mur (voir point 1 avant).

--- Projet3/GameScene.swift
+++ Projet3/GameScene.swift
@@ -18,6 +18,8 @@ class GameScene: SKScene {
     var construireMur = false
     let chemin = CGPathCreateMutable()
     var murTemp = SKNode()
+    var deplacement = false
+    var nodeQuiSeDeplace = SKSpriteNode()
     //Les variables de son
     var sonCorbeille: SystemSoundID = 0
     var sonObjSurTable: SystemSoundID = 0
@@ -39,7 +41,8 @@ class GameScene: SKScene {
         "boutonpaletteGauche2",
         "boutonportail",
         "boutonressort",
-        "boutontrou"]
+        "boutontrou",
+        "boutondeplacement"]
     let listeDesObjets = [
         "accelerateur",
         "butoirCirc",
@@ -74,101 +77,34 @@ class GameScene: SKScene {
         CGPathMoveToPoint(chemin, nil, position1.x, position1.y)
         //finTest
         
-        for touch in (touches ) {
+        for touch in touches {
             let location = touch.locationInNode(self)
-            let touchedNode = self.nodeAtPoint(location)
-
-            if let name = touchedNode.name
+            let touchedNode = nodeAtPoint(location)
+            
+            if listeDesObjets.contains(touchedNode.name!)
             {
-                if name == "boutonmur" {
-                    construireMur = true
-                    // jouer un son
-                    AudioServicesPlaySystemSound(sonSelectionOutil);
-                }
-                
-                if !listeDesObjets.contains(name)
+                if let monNode = touchedNode as? SKSpriteNode
                 {
-                    if selection
-                    {
-                        if name == "save_select"
-                        {
-                            savedSelected = nodesSelected
-                        }
-                        
-                        if name == "same_select" && nodesSelected.count == 1
-                        {
-                            for enfant in self.children
-                            {
-                                if enfant.name == nodesSelected[0].name
-                                {
-                                    if let monEnfant = enfant as? SKSpriteNode
-                                    {
-                                        nodesSelected.append(monEnfant)
-                                        monEnfant.alpha = 0.5
-                                    }
-                                }
-                            }
-                        }else
-                        {
-                            selection = false
-                            updateVisibiliteCorbeille()
-                            if name == "boutonDelete"
-                            {
-                                for node in nodesSelected
-                                {
-                                    savedSelected = savedSelected.filter {$0 != node}
-                                    node.removeFromParent()
-                                }
-                                
-                                // jouer un son
-                                AudioServicesPlaySystemSound(sonCorbeille);
-                            }
-                            for node in nodesSelected
-                            {
-                                node.alpha = 1
-                            }
-                            nodesSelected.removeAll()
-                        }
-                    }else
-                    {
-                        if listeDesBoutons.contains(name){
-                            construireMur = false
-                            let nomObjet = name.substringFromIndex(name.startIndex.advancedBy(6))
-                            sprite = SKSpriteNode(imageNamed: nomObjet)
-                            sprite.name = nomObjet
-                            // jouer un son
-                            AudioServicesPlaySystemSound(sonSelectionOutil);
-                        }else if name == "table" && sprite.name != "Spaceship" && !construireMur
-                        {
-                            sprite.xScale = 0.5
-                            sprite.yScale = 0.5
-                            sprite.position = location
-                            
-                            self.addChild(sprite.copy() as! SKNode)
-                            
-                            // jouer un son
-                            AudioServicesPlaySystemSound(sonObjSurTable);
-                        }else if name == "load_select"
-                        {
-                            nodesSelected = savedSelected
-                            if nodesSelected.count > 0
-                            {
-                                for node in nodesSelected
-                                {
-                                    node.alpha = 0.5
-                                }
-                                selection = true
-                                updateVisibiliteCorbeille()
-                            }
-                        }
-                    }
-                }else
-                {
-                    cliqueAutreQueBouton(touchedNode)
+                    nodeQuiSeDeplace = monNode
                 }
-            }else
+            }
+            
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            
+            //Ne permet que le déplacement d'un node a la fois pour le moment
+            //Autre bug: Si on bouge trop vite, le touch sort de l'objet et ça marche pas
+            if let nomDuNode = nodeQuiSeDeplace.name
             {
-                cliqueAutreQueBouton(touchedNode)
+                if listeDesObjets.contains(nomDuNode) && !construireMur
+                {
+                    nodeQuiSeDeplace.position = location
+                    deplacement = true
+                }
             }
         }
     }
@@ -194,14 +130,137 @@ class GameScene: SKScene {
             !construireMur
         }
         
-        
+        if deplacement
+        {
+            deplacement = false
+            nodeQuiSeDeplace = SKSpriteNode()
+        }else
+        {
+            
+            for touch in (touches ) {
+                let location = touch.locationInNode(self)
+                let touchedNode = self.nodeAtPoint(location)
+                
+                if let name = touchedNode.name
+                {
+                    
+                    if name == "boutonmur" {
+                        construireMur = true
+                        // jouer un son
+                        AudioServicesPlaySystemSound(sonSelectionOutil);
+                    }
+                    
+                    if !listeDesObjets.contains(name)
+                    {
+                        
+                        if selection
+                        {
+                            if name == "save_select"
+                            {
+                                savedSelected = nodesSelected
+                            }
+                            
+                            if name == "same_select" && nodesSelected.count == 1
+                            {
+                                for enfant in self.children
+                                {
+                                    if enfant.name == nodesSelected[0].name
+                                    {
+                                        if let monEnfant = enfant as? SKSpriteNode
+                                        {
+                                            nodesSelected.append(monEnfant)
+                                            monEnfant.alpha = 0.5
+                                        }
+                                    }
+                                }
+                            }else
+                            {
+                                selection = false
+                                updateVisibiliteCorbeille()
+                                if name == "boutonDelete"
+                                {
+                                    for node in nodesSelected
+                                    {
+                                        savedSelected = savedSelected.filter {$0 != node}
+                                        node.removeFromParent()
+                                    }
+                                    
+                                    // jouer un son
+                                    AudioServicesPlaySystemSound(sonCorbeille);
+                                }
+                                for node in nodesSelected
+                                {
+                                    node.alpha = 1
+                                }
+                                nodesSelected.removeAll()
+                                
+                                cliqueSurBoutonObj(name)
+                            }
+                        }else
+                        {
+                            if !cliqueSurBoutonObj(name) && name == "table" && sprite.name != "Spaceship" && !construireMur
+                            {
+                                sprite.xScale = 0.5
+                                sprite.yScale = 0.5
+                                sprite.position = location
+                                
+                                sprite.physicsBody = SKPhysicsBody.init(texture: sprite.texture!, alphaThreshold: 0.5, size: sprite.size)
+                                
+                                sprite.physicsBody?.affectedByGravity = false
+                                sprite.physicsBody?.allowsRotation = false
+                                
+                                
+                                self.addChild(sprite.copy() as! SKSpriteNode)
+                                
+                                // jouer un son
+                                AudioServicesPlaySystemSound(sonObjSurTable);
+                            }else if name == "load_select"
+                            {
+                                nodesSelected = savedSelected
+                                if nodesSelected.count > 0
+                                {
+                                    for node in nodesSelected
+                                    {
+                                        node.alpha = 0.5
+                                    }
+                                    selection = true
+                                    updateVisibiliteCorbeille()
+                                }
+                            }
+                        }
+                    }else
+                    {
+                        cliqueAutreQueBouton(touchedNode, location: location)
+                    }
+                }else
+                {
+                    cliqueAutreQueBouton(touchedNode, location: location)
+                }
+            }
+        }
     }
     
-    func cliqueAutreQueBouton(touchedNode: SKNode){
-        //TODO: Faire une deuxième array pour les SKShape nodes et faire le meme test
-        //      pour ajouter un mur à une sélection dans un array de shapes.
+    func cliqueSurBoutonObj(name: String) -> Bool{
+        if listeDesBoutons.contains(name){
+            construireMur = false
+            let nomObjet = name.substringFromIndex(name.startIndex.advancedBy(6))
+            sprite = SKSpriteNode(imageNamed: nomObjet)
+            sprite.name = nomObjet
+            // jouer un son
+            AudioServicesPlaySystemSound(sonSelectionOutil);
+            return true
+        }
+        return false
+    }
+    
+    func cliqueAutreQueBouton(touchedNode: SKNode, location: CGPoint){
+        
         if let objSelectionne = touchedNode as? SKSpriteNode
         {
+            if touchedNode.containsPoint(location){
+                
+            let unPetitTest: CGRect = objSelectionne.calculateAccumulatedFrame()
+            if (CGRectContainsPoint(unPetitTest, location)){
             if nodesSelected.contains(objSelectionne)
             {
                 nodesSelected = nodesSelected.filter {$0 != objSelectionne}
@@ -220,6 +279,8 @@ class GameScene: SKScene {
             }
             // jouer un son
             AudioServicesPlaySystemSound(sonSelection);
+                }
+            }
         }
     }
     
