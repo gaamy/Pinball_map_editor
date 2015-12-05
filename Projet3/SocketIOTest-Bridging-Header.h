9ea68332daf3612ee9eef47364d8b4c8fc86c71c
gaamy <gabriel.amyot@gmail.com>
2015-12-05 10:17:31 -0500

    xcworkspace

---
+++ Projet3/SocketIOTest-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+#ifndef SocketIOTest_SocketIOTest_Bridging-Header_h
+#define SocketIOTest_SocketIOTest_Bridging-Header_h
+
+
+#import "SRWebSocket.h"
+
+#import "SocketIO.h"
+#import "SocketIOPacket.h"
+#import "SocketIOTransport.h"
+#import "SocketIOTransportWebsocket.h"
+#import "SocketIOTransportXHR.h"
+
+#endif
